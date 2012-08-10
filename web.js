b59b86c707e8f37078a56fb27123a52cc1138106
Max Kueng <me@maxkueng.com>
2012-08-10 21:44:51 +0200

    init

---
+++ web.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var express = require('express');
+
+var app = express.createServer(
+//	express.logger(), 
+	express.static(__dirname + '/public'), 
+	express.bodyParser(), 
+	express.cookieParser()
+);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log('App running on port ' + port);
+});
+
+app.get('/', function (req, res) {
+	res.send('OK');
+});
