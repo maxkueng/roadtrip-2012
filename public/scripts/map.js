18fc615bae22012cf98d1a34e73a9a0dbf5fe49e
Max Kueng <me@maxkueng.com>
2012-08-13 19:56:52 +0200

    stage rect

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -9,54 +9,99 @@ $(document).ready(function () {
 
 	var defaultStyle = {
 		'color' : '#2262CC',
-		'weight' : 4,
+		'weight' : 6,
+		'opacity' : 0.8
+	};
+
+	var highlightStyle2 = {
+		'color' : '#33CC66',
+		'weight' : 6,
 		'opacity' : 0.8
 	};
 
 	var highlightStyle = {
 		'color' : '#FF0033', 
-		'weight' : 4,
-		'opacity' : 0.8
+		'weight' : 6,
+		'opacity' : 0.6,
+		'fillColor' : 'transparent'
 	};
+
+	var rectStyle = {
+		'color' : 'transparent', 
+		'fillColor' : 'transparent'
+	};
+
 	var featureLayer;
-	var layers = {};
+	var stages = {};
 
-	var fetchFullLayer = function (layer, properties) {
-		$('#loader').loader('start');
-		map.fitBounds(layer.getBounds());
+	map.on('zoomed', function () {
+		// map.getZoom();
+	});
 
-		setTimeout(function () {
-			$.getJSON('/data/track/' + properties.name + '-full.json', function (collection) {
-				featureLayer.addData(collection);
-			});
-		}, 0);
+	var fetchFullLayer = function (name) {
+		var layer = stages[name].normal;
+
+		if (stages.hasOwnProperty(name) && stages[name].full === null) {
+			$('#loader').loader('start');
+			map.fitBounds(layer.getBounds());
+
+			setTimeout(function () {
+				$.getJSON('/data/track/' + name + '-full.json', function (collection) {
+					featureLayer.addData(collection);
+				});
+			}, 0);
+
+		} else {
+			map.fitBounds(layer.getBounds());
+		}
+	};
+
+	var highlightLayer = function (name, highlight) {
+		var layer, style;
+
+		if (stages[name].full !== null) {
+			layer = stages[name].full;
+		} else {
+			layer = stages[name].normal;
+		}
+
+		style = (highlight === true) ? highlightStyle : defaultStyle;
+		layer.setStyle(style);
 	};
 
 	var onEachFeature = function (feature, layer) {
+		var layerName = feature.properties.name;
 		layer.setStyle(defaultStyle);
 
-		if (layers.hasOwnProperty(feature.properties.name)) {
-			featureLayer.removeLayer(layers[feature.properties.name]);
+		if (!stages.hasOwnProperty(layerName)) {
+			stages[layerName] = {
+				'normal' : layer,
+				'full' : null
+			};
+
+			var rect = new L.Rectangle(layer.getBounds());
+			rect.setStyle(rectStyle);
+
+			rect.on('mouseover', function (e) {
+				highlightLayer(layerName, true);
+			});
+
+			rect.on('mouseout', function (e) {
+				highlightLayer(layerName, false);
+			});
+
+			rect.on('click', function (e) {
+				fetchFullLayer(layerName);
+			});
+
+			map.addLayer(rect);
+
+		} else {
+			featureLayer.removeLayer(stages[layerName].normal);
 			$('#loader').loader('stop');
-			delete layers[feature.properties.name];
+			stages[layerName].full = layer;
+//			delete stages[layerName].normal;
 		}
-
-		layers[feature.properties.name] = layer;
-
-		(function(layer, properties) {
-			layer.on("mouseover", function (e) {
-				layer.setStyle(highlightStyle);
-			});
-
-			layer.on("mouseout", function (e) {
-				layer.setStyle(defaultStyle); 
-			});
-
-			layer.on('click', function (e) {
-				fetchFullLayer(layer, properties);
-			});
-
-		})(layer, feature.properties);
 	};
 
 	featureLayer = L.geoJson(null, {
@@ -66,33 +111,36 @@ $(document).ready(function () {
 
 	var getStages = function () {
 		$.getJSON('/data/stages.json', function(data) {
-			var i, len;
+			var i, len, count;
+			count = 0;
 			for (i = 0, len = data.length; i < len; i++) {
-				(function (_i) {
-					$.getJSON('/data/track/' + data[_i] + '.json', function (collection) {
-						featureLayer.addData(collection);
-						if (_i === (len -1)) {
-							map.fitBounds(featureLayer.getBounds());
-						}
-					});
-				})(i);
+				$.getJSON('/data/track/' + data[i] + '.json', function (collection) {
+					featureLayer.addData(collection);
+					count++;
+					if (count === (len -1)) {
+						map.fitBounds(featureLayer.getBounds());
+					}
+				});
 			}
 		});
 	};
 
 	var getPhotos = function () {
+		var oms = new OverlappingMarkerSpiderfier(map);
 
-	var photoMarkers = new L.MarkerClusterGroup();
 		$.getJSON('/data/photos.json', function (data) {
 			var i, len;
 			for (i = 0, len = data.length; i < len; i++) {
 				var marker = new L.Marker(new L.LatLng(data[i].latitude, data[i].longitude), { 
-					'title' : 'photo'
+					'title' : 'photo',
+					'icon' : new L.Icon({
+						'iconUrl' : data[i].url_sq,
+						'iconSize' : [22, 22]
+					})
 				});
-				marker.bindPopup('<a href="' + data[i].pageUrl + '"><img src="' + data[i].url_sq + '" /></a>');
-				photoMarkers.addLayer(marker);
+				marker.addTo(map);
+				oms.addMarker(marker);
 			}
-			map.addLayer(photoMarkers);
 		});
 	};
 
